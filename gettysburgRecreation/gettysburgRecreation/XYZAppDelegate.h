2fb8e826038b91bc11dde1695093dbd9c0bcb1cc
Ellen Hui <ellenfkh@gmail.com>
2014-02-08 13:08:31 -0800

    add the xCode app folder

---
+++ gettysburgRecreation/gettysburgRecreation/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  gettysburgRecreation
+//
+//  Created by Ellen Hui on 2/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gettysburgRecreation/gettysburgRecreation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gettysburgRecreation
+//
+//  Created by Ellen Hui on 2/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
