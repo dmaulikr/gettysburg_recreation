55482874e90ecd7bde86c013a5efd0d9c52331c2
Ellen Hui <ellenfkh@gmail.com>
2014-02-10 21:57:18 -0800

    add notes to plan, fiddle with board

--- gettysburgRecreation/Constants.h
+++ gettysburgRecreation/Constants.h
@@ -11,8 +11,11 @@
 #ifndef gettysburgRecreation_Constants_h
 
 // Board Sizes
-#define BOARD_WIDTH int 100
-#define BOARD_HEIGHT int 100
+//#define BOARD_WIDTH int 100
+//#define BOARD_HEIGHT int 100
+
+extern const int BOARD_WIDTH;
+extern const int BOARD_HEIGHT;
 
 #define gettysburgRecreation_Constants_h
 
