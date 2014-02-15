9db37393a8aa6eb8d07d6a08cc656348aac6e1a8
Ellen Hui <ellenfkh@gmail.com>
2014-02-15 14:50:04 -0800

    Added getter + bounds checking to board (get tile)

--- gettysburgRecreation/XYZTile.m
+++ gettysburgRecreation/XYZTile.m
@@ -10,7 +10,7 @@
 
 @implementation XYZTile
 
-- (id)initWithValues:(float)elevation movementSpeedMultiplier:(float)movementSpeedMultiplier combatMultiplier:(float)combatMultiplier terrain:(NSString *)terrain{
+- (id)initWithValues:(float)elevation movementSpeedMultiplier:(float)movementSpeedMultiplier combatMultiplier:(float)combatMultiplier terrain:(TileTerrain)terrain{
     self = [super init];
     if (self) {
         self.elevation = elevation;
