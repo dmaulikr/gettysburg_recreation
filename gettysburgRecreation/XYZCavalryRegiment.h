cd7fa74290fe78284da3016592ae1c2be4e68262
Ellen Hui <ellenfkh@gmail.com>
2014-02-20 09:14:45 -0800

    create subclasses of regiment

---
+++ gettysburgRecreation/XYZCavalryRegiment.h
@@ -0,0 +1,13 @@
+//
+//  XYZCavalryRegiment.h
+//  gettysburgRecreation
+//
+//  Created by Ellen Hui on 2/20/14.
+//
+//
+
+#import "XYZRegiment.h"
+
+@interface XYZCavalryRegiment : XYZRegiment
+
+@end
---
+++ gettysburgRecreation/XYZInfantryRegiment.m
@@ -0,0 +1,13 @@
+//
+//  XYZInfantryRegiment.m
+//  gettysburgRecreation
+//
+//  Created by Ellen Hui on 2/20/14.
+//
+//
+
+#import "XYZInfantryRegiment.h"
+
+@implementation XYZInfantryRegiment
+
+@end
