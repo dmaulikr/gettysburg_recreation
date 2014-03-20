5690a35ee2980b219b588735858d55346e5a71c2
Ellen Hui <ellenfkh@gmail.com>
2014-03-19 17:45:50 -0700

    forgot to add these files

---
+++ gettysburg-recreation-2/gettysburg-recreation-2/gettysburg-recreation-2/GBGMainViewController.h
@@ -0,0 +1,14 @@
+//
+//  GBGMainViewController.h
+//  gettysburg-recreation-2
+//
+//  Created by Ellen Hui on 3/19/14.
+//  Copyright (c) 2014 Harvey Mudd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBGMainViewController : UIViewController
+
+
+@end
