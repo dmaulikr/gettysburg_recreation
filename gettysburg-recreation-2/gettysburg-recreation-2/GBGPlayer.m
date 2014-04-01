5739dfc9760eb80edbe00f6c735210c72d0ee8f1
Ellen Hui <ellenfkh@gmail.com>
2014-03-31 19:42:17 -0700

    Added a player, sprite files.  Transition to main view now creates a GBGPlayer.

---
+++ gettysburg-recreation-2/gettysburg-recreation-2/GBGPlayer.m
@@ -0,0 +1,13 @@
+//
+//  GBGPlayer.m
+//  gettysburg-recreation-2
+//
+//  Created by Ellen Hui on 3/31/14.
+//  Copyright (c) 2014 Harvey Mudd. All rights reserved.
+//
+
+#import "GBGPlayer.h"
+
+@implementation GBGPlayer
+
+@end
