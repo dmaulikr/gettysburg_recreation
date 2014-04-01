d4d35819e3819ed99bbeb49df1271600c3ad4531
Ellen Hui <ellenfkh@gmail.com>
2014-03-31 22:01:11 -0700

    added basic regiments.

---
+++ gettysburg-recreation-2/gettysburg-recreation-2/GBGRegiment.h
@@ -0,0 +1,13 @@
+//
+//  GBGRegiment.h
+//  gettysburg-recreation-2
+//
+//  Created by Ellen Hui on 3/31/14.
+//  Copyright (c) 2014 Harvey Mudd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBGRegiment : NSObject
+
+@end
---
+++ gettysburg-recreation-2/gettysburg-recreation-2/GBGRegiment.m
@@ -0,0 +1,13 @@
+//
+//  GBGRegiment.m
+//  gettysburg-recreation-2
+//
+//  Created by Ellen Hui on 3/31/14.
+//  Copyright (c) 2014 Harvey Mudd. All rights reserved.
+//
+
+#import "GBGRegiment.h"
+
+@implementation GBGRegiment
+
+@end
